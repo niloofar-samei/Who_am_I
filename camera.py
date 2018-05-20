a1b273103fbe8f63ec44eeb3fa7b6fe105dd4fec
niloofar <niloofar.samei@gmail.com>
2018-05-21 01:09:11 +0430

    commented the line that close the webcam after a few seconds

--- camera.py
+++ camera.py
@@ -4,14 +4,19 @@ cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
+	rval, frame = vc.read()
 else:
-    rval = False
+	rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+	cv2.imshow("preview", frame)
+	rval, frame = vc.read()
+	key = cv2.waitKey(5)
+
+# close webcam 3000ms
+#	cv2.waitKey(3000)
+#	cv2.destroyAllWindows("preview")
+
+	if key == 27 : # exit on ESC
+		break
 cv2.destroyWindow("preview")
